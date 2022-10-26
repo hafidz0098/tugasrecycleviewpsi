6500455026d5b8d4767d0bba55060644a71bffda
hafidz0098 <hafidznak123@gmail.com>
2022-10-26 23:06:22 +0700

    implement member mahasiswa adapter

--- app/src/main/java/com/example/tugasrecycleviewpsi/MainActivity.kt
+++ app/src/main/java/com/example/tugasrecycleviewpsi/MainActivity.kt
@@ -2,16 +2,23 @@ package com.example.tugasrecycleviewpsi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerview: RecyclerView
+    lateinit var adapter: MahasiswaAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        recyclerview.adapter = adapter
     }
 
     private fun init(){
@@ -21,5 +28,7 @@ class MainActivity : AppCompatActivity() {
         data.add(Mahasiswa(R.drawable.alfian, "Nama : Alfian Tri Wahyu","NIM : 205150201111049", "Prodi : Teknik Informatika"))
         data.add(Mahasiswa(R.drawable.alfian, "Nama : Hafidz","NIM : 205150201111064", "Prodi : Teknik Informatika"))
         data.add(Mahasiswa(R.drawable.alfian, "Nama : Patrick Juan","NIM : 20515020011106", "Prodi : Teknik Informatika"))
+
+        adapter = MahasiswaAdapter(data)
     }
 }
