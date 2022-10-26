73861635375b6e75ab001f1130f52d388fe1726f
hafidz0098 <hafidznak123@gmail.com>
2022-10-26 22:44:17 +0700

    add list mahasiswa (Hafidz)

--- app/src/main/java/com/example/tugasrecycleviewpsi/MainActivity.kt
+++ app/src/main/java/com/example/tugasrecycleviewpsi/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.tugasrecycleviewpsi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerview: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun init(){
+        recyclerview = findViewById(R.id.recyclerview)
+
+        var data = ArrayList<Mahasiswa>()
+        data.add(Mahasiswa(R.drawable.alfian, "Nama : Alfian Tri Wahyu","NIM : 205150201111049", "Prodi : Teknik Informatika"))
+        data.add(Mahasiswa(R.drawable.alfian, "Nama : Hafidz","NIM : 205150201111064", "Prodi : Teknik Informatika"))
+        data.add(Mahasiswa(R.drawable.alfian, "Nama : Patrick Juan","NIM : 20515020011106", "Prodi : Teknik Informatika"))
     }
 }
